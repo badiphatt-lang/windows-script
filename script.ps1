687ee3bc4bb3cdfd086d6993db820b8b55b7d1b9
badiphatt-lang <badiphatt@gmail.com>
2026-03-09 21:27:06 +0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,11 +1,20 @@
+# Check admin
+if (-not ([Security.Principal.WindowsPrincipal] 
+[Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(
+[Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -Command `"irm https://raw.githubusercontent.com/badiphat-lang/windows-script/main/script.ps1 | iex`"" -Verb RunAs
+exit
+}
+
 $securePass = Read-Host "Enter Password" -AsSecureString
 $pass = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
-    [Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePass)
+[Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePass)
 )
 
 if ($pass -ne "dotexe") {
-    Write-Host "Warning Password"
-    exit
+Write-Host "Wrong Password"
+exit
 }
 
 Write-Host "Running Script..."

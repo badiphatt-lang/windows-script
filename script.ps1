4e5df87303be1ee8a40b5b5cd850ecee23a28fd1
badiphatt-lang <badiphatt@gmail.com>
2026-03-09 21:32:41 +0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,25 +1,25 @@
-# Check admin
-if (-not ([Security.Principal.WindowsPrincipal] 
-[Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(
-[Security.Principal.WindowsBuiltInRole] "Administrator"))
-{
-Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -Command `"irm https://raw.githubusercontent.com/badiphat-lang/windows-script/main/script.ps1 | iex`"" -Verb RunAs
-exit
+# Run as Admin
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -Command `"iwr -useb https://raw.githubusercontent.com/badiphat-lang/windows-script/main/script.ps1 | iex`"" -Verb RunAs
+    exit
 }
 
+# Password (hidden)
 $securePass = Read-Host "Enter Password" -AsSecureString
-$pass = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
-[Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePass)
-)
+$pass = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePass))
 
 if ($pass -ne "dotexe") {
-Write-Host "Wrong Password"
-exit
+    Write-Host "Wrong Password" -ForegroundColor Red
+    exit
 }
 
-Write-Host "Running Script..."
+Write-Host "Running Script..." -ForegroundColor Cyan
+
+# ===== Tweaks =====
 
 powercfg -setactive SCHEME_MIN
 netsh int tcp set global autotuninglevel=disabled
 
-Write-Host "Ok."
+# ==================
+
+Write-Host "Ok." -ForegroundColor Green
